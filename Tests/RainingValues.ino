0fcab274779dc7c00a08e42b32aa392091b67d49
Lukas Alberto Belck <lukas.belck@gmail.com>
2017-02-20 23:36:14 -0300

    Rain Sensor test
    Just a simple arduino sketch that display the analog and digital values of the rain sensor

---
+++ Tests/RainingValues.ino
@@ -0,0 +1,18 @@
+byte aRainPin = A1;
+byte dRainPin = 6;
+
+void setup() {
+  pinMode(aRainPin, INPUT);
+  pinMode(dRainPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int aSensorValue = analogRead(aRainPin);
+  byte dSensorValue = digitalRead(dRainPin);
+  Serial.print("Analog Value: ");
+  Serial.println(aSensorValue);
+  Serial.print("Digital Value: ");
+  Serial.println(dSensorValue);
+  delay(300);
+}
