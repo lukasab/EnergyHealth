5a36153594f9b3607d3b202037d1e92d6c236e7c
Lukas Alberto Belck <lukas.belck@gmail.com>
2017-02-21 23:18:23 -0300

    Add files via upload
    memory stuff and comments

--- Tests/SendingEmail.ino
+++ Tests/SendingEmail.ino
@@ -7,49 +7,50 @@ byte mac[] = { 0x54, 0x34, 0x41, 0x30, 0x30, 0x31 };
 // For the rest we use DHCP (IP address and such)
 
 EthernetClient client;
-char server[] = "www.wiplink.com.br"; // IP Adres (or name) of server to dump data to
+char server[] = "www.yoursite.com"; // IP Adres (or name) of server to dump data to
 int  interval = 10000; // Wait time between emails 
-int i = 0;
 
 void setup() {
 
   Serial.begin(9600);
-  Serial.println("Program: Send Email");
+  Serial.println(F("Program: Send Email"));
 
   Ethernet.begin(mac);
   delay(1000);
-  Serial.print("IP Address: ");
+  Serial.print(F("IP Address: "));
   Serial.println(Ethernet.localIP());
-  Serial.print("Subnet Mask: ");
+  Serial.print(F("Subnet Mask: "));
   Serial.println(Ethernet.subnetMask());
-  Serial.print("Default Gateway IP: ");
+  Serial.print(F("Default Gateway IP: "));
   Serial.println(Ethernet.gatewayIP());
-  Serial.print("DNS Server IP: ");
+  Serial.print(F("DNS Server IP: "));
   Serial.println(Ethernet.dnsServerIP());
 }
 
 void loop() {
   // if you get a connection, report back via serial:
   if (client.connect(server, 80)) {
-    Serial.println("-> Connected");
-    i++;
+    Serial.println(F("-> Connected"));
     // Make a HTTP request:
-    client.print("GET /wimax/ardemail.php?");
-    client.print("FromName=arduino&");
-    client.print("Assunto=Loop");
-    client.print(i);
+    client.print("GET /SendEmail.php?");     //where your php file is (www.yoursite.com/SendEmail.php?)
+    client.print("Recipient=");
+    client.print("your.email@gmail.com");    //email to send to 
     client.print("&");
-    client.print("MailMsg=ola");
+    client.print("Subject=");
+    client.print("Arduino");                 //subject of email
+    client.print("&");
+    client.print("Message=");
+    client.print("MessageofArduino");         //message of email
     client.println( " HTTP/1.1");
     client.print( "Host: " );
     client.println(server);
     client.println( "Connection: close" );
     client.println();
-    client.stop();
+    client.stop();    
   }
   else {
     // you didn't get a connection to the server:
-    Serial.println("--> connection failed/n");
+    Serial.println(F("--> connection failed/n"));
   }
 
   delay(interval);
